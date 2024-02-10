f3deba3a80ba5d08ed60e90db35d2308c5f2c668
Roman Romenskiy <154092982+rromenskyi@users.noreply.github.com>
2024-02-10 06:04:30 +0100

    audiofork.py

--- agi-bin/audiofork.py
+++ agi-bin/audiofork.py
@@ -9,6 +9,40 @@ from google.cloud import speech_v1p1beta1
 from google.oauth2 import service_account
 import phonenumbers
 from phonenumbers import geocoder, carrier
+import time
+import asterisk.manager
+
+def find_and_hangup(call_number):
+    # Устанавливаем соединение с AMI
+    manager = asterisk.manager.Manager()
+
+
+    try:
+        # Подключаемся к серверу Asterisk AMI
+        manager.connect('ip')
+        manager.login('login', 'password')
+
+        # Получаем список активных каналов
+        response = manager.send_action({'Action': 'Command', 'Command': 'core show channels concise'})
+
+        # Разбиваем строку ответа на строки итерацией по ним
+        for line in response.data.splitlines():
+            # Разделяем строку по пробелам
+            parts = line.split('!')
+#            print(line)
+            if len(parts) >= 10:
+                # Получаем номер телефона из строки
+                channel = parts[0]
+                if call_number in line:
+                    # Если найденный канал соответствует номеру, отправляем ему hangup
+                    manager.send_action({'Action': 'Hangup', 'Channel': channel})
+                    print(f"Hangup sent to {channel}")
+                    break
+    except Exception as e:
+        print("An error occurred:", e)
+    finally:
+        # Закрываем соединение
+        manager.close()
 
 def validate_phone_number(phone_number):
     try:
@@ -115,7 +149,10 @@ async def handle_audio(websocket, path):
             await process_audio(audio_data,text_array)
             print(text_array)
             if count_matching_arrays(keywords, text_array, 2):
-                print('Answering machine detected')
+                if phone.startswith('+'):
+                    phone  = phone_number.lstrip('+')
+                    find_and_hangup(phone_number)
+                    print('Answering machine detected')
 
     except websockets.exceptions.ConnectionClosedError:
         print("WebSocket connection closed by client")
