7ab637db4888e0195061e198ab8cd8c0ceabbc52
Roman Romenskiy <154092982+rromenskyi@users.noreply.github.com>
2024-02-26 11:11:19 +0100

    tuneup

--- dockerfiles/compose/up.sh
+++ dockerfiles/compose/up.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-mkdir -p ./asterisk/logs
-mkdir -p ./asterisk/etc
-mkdir -p ./mariadb/data
-mkdir -p ./html
-cd html
+mkdir -p ./runtime/asterisk/logs
+mkdir -p ./runtime/asterisk/etc
+mkdir -p ./runtime/mariadb/data
+mkdir -p ./runtime/html
+cd runtime/html
 git clone https://github.com/rromenskyi/Asterisk-PHP-CDR .
-cp ../html-config/config.php inc/config/
+cp ../runtime/html-config/config.php inc/config/
+cd ..
 cd ..
 
 docker-compose up -d
